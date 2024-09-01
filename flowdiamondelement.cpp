90f0b990d223910ab8c55a60e0e6e905c5c31eff
Ouzhendong <460917227@qq.com>
2024-09-01 20:17:16 +0800

    缩放函数基本重写完成，还剩一些bug

--- flowdiamondelement.cpp
+++ flowdiamondelement.cpp
@@ -4,13 +4,13 @@
 
 FlowDiamondElement::FlowDiamondElement(): FlowElement()
 {
-    borderDots.append(new QGraphicsRectItem(QRectF(0, 0, DOT_SIZE, DOT_SIZE), this));  // 左上角
+    borderDots.append(new QGraphicsRectItem(QRectF(0, 0, DOT_SIZE, DOT_SIZE), this));  // 下
     borderDots.last()->setPos(250, 200);
-    borderDots.append(new QGraphicsRectItem(QRectF(0, 0, DOT_SIZE, DOT_SIZE), this));  // 右上角
+    borderDots.append(new QGraphicsRectItem(QRectF(0, 0, DOT_SIZE, DOT_SIZE), this));  // 左
     borderDots.last()->setPos(150, 150);
-    borderDots.append(new QGraphicsRectItem(QRectF(0, 0, DOT_SIZE, DOT_SIZE), this));  // 右下角
+    borderDots.append(new QGraphicsRectItem(QRectF(0, 0, DOT_SIZE, DOT_SIZE), this));  // 上
     borderDots.last()->setPos(250, 100);
-    borderDots.append(new QGraphicsRectItem(QRectF(0, 0, DOT_SIZE, DOT_SIZE), this));  // 左下角
+    borderDots.append(new QGraphicsRectItem(QRectF(0, 0, DOT_SIZE, DOT_SIZE), this));  // 右
     borderDots.last()->setPos(350, 150);
 
     controlDots = borderDots;
@@ -18,3 +18,29 @@ FlowDiamondElement::FlowDiamondElement(): FlowElement()
     // 绘制路径
     draw();
 }
+void FlowDiamondElement::scale(int index, double dx, double dy)
+{
+    qDebug() << "开始缩放";
+
+    if (index == 0) {  // 下
+        controlDots.at(0)->moveBy(0, dy);
+        controlDots.at(1)->moveBy(0, dy*0.5);
+        controlDots.at(3)->moveBy(0, dy*0.5);
+    } else if (index == 1) {  // 左
+        controlDots.at(1)->moveBy(dx, 0);
+        controlDots.at(0)->moveBy(dx*0.5, 0);
+        controlDots.at(2)->moveBy(dx*0.5, 0);
+    } else if (index == 2) {  // 上
+        controlDots.at(2)->moveBy(0, dy);
+        controlDots.at(1)->moveBy(0, dy*0.5);
+        controlDots.at(3)->moveBy(0, dy*0.5);
+    } else if (index == 3) {  // 右
+        controlDots.at(3)->moveBy(dx, 0);
+        controlDots.at(0)->moveBy(dx*0.5, 0);
+        controlDots.at(2)->moveBy(dx*0.5, 0);
+    }
+
+    // 重绘图形
+    draw();
+}
+
