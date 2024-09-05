aa77b52a2effc9debd591aea4f0142e6ae19cff0
baibai1906 <wang2004630@163.com>
2024-09-05 19:43:24 +0800

    美的胜利

--- flowdocuelement.cpp
+++ flowdocuelement.cpp
@@ -69,147 +69,147 @@ void FlowDocuElement::draw() {
 void FlowDocuElement::mySetScale(int index, double dx, double dy)
 {
 
-    if(!inBorder(index)[0]||!inBorder(index)[1])//若越界
-    {
-        qDebug()<<"越界了";
-        int oppIdx = (index+2) % 4;//对角点
-        if((!inBorder(index)[0])&&(!inBorder(index)[1])){//xy都越界
-            qDebug()<<"xy都越界";
-            dx = deltax[index];
-            dy = deltay[index];
-            double x = controlDots.at(oppIdx)->scenePos().x();
-            double y = controlDots.at(oppIdx)->scenePos().y();
-            controlDots.at(index)->setPos(x+dx,y+dy);
-            if(index==0){
-                borderDots.at(1)->setPos(x+0, y+dy);
-                borderDots.at(3)->setPos(x+dx, y+0);
+    // if(!inBorder(index)[0]||!inBorder(index)[1])//若越界
+    // {
+    //     qDebug()<<"越界了";
+    //     int oppIdx = (index+2) % 4;//对角点
+    //     if((!inBorder(index)[0])&&(!inBorder(index)[1])){//xy都越界
+    //         qDebug()<<"xy都越界";
+    //         dx = deltax[index];
+    //         dy = deltay[index];
+    //         double x = controlDots.at(oppIdx)->scenePos().x();
+    //         double y = controlDots.at(oppIdx)->scenePos().y();
+    //         controlDots.at(index)->setPos(x+dx,y+dy);
+    //         if(index==0){
+    //             borderDots.at(1)->setPos(x+0, y+dy);
+    //             borderDots.at(3)->setPos(x+dx, y+0);
 
-                // 过渡点
-                borderDots.at(4)->setPos(x+dx * 0.25, y+dy * 0.2);  // 过渡点1
-                borderDots.at(5)->setPos(x+dx * 0.5, y+0);          // 过渡点2
-                borderDots.at(6)->setPos(x+dx * 0.75, y-dy * 0.2); // 过渡点3
-            }
-            if(index==1){
-                borderDots.at(0)->setPos(x+0, y+dy);
-                borderDots.at(2)->setPos(x+dx, y+0);
+    //             // 过渡点
+    //             borderDots.at(4)->setPos(x+dx * 0.25, y+dy * 0.2);  // 过渡点1
+    //             borderDots.at(5)->setPos(x+dx * 0.5, y+0);          // 过渡点2
+    //             borderDots.at(6)->setPos(x+dx * 0.75, y-dy * 0.2); // 过渡点3
+    //         }
+    //         if(index==1){
+    //             borderDots.at(0)->setPos(x+0, y+dy);
+    //             borderDots.at(2)->setPos(x+dx, y+0);
 
-                // 过渡点
-                borderDots.at(4)->setPos(x+dx * 0.75, y+dy * 0.2);  // 过渡点1
-                borderDots.at(5)->setPos(x+dx * 0.5, y+0);          // 过渡点2
-                borderDots.at(6)->setPos(x+dx * 0.25, y-dy * 0.2); // 过渡点3
+    //             // 过渡点
+    //             borderDots.at(4)->setPos(x+dx * 0.75, y+dy * 0.2);  // 过渡点1
+    //             borderDots.at(5)->setPos(x+dx * 0.5, y+0);          // 过渡点2
+    //             borderDots.at(6)->setPos(x+dx * 0.25, y-dy * 0.2); // 过渡点3
 
-            }
-            if(index==2){
-                borderDots.at(3)->setPos(x+0, y+dy);
-                borderDots.at(1)->setPos(x+dx, y+0);
+    //         }
+    //         if(index==2){
+    //             borderDots.at(3)->setPos(x+0, y+dy);
+    //             borderDots.at(1)->setPos(x+dx, y+0);
 
-                // 过渡点
-                borderDots.at(4)->setPos(x+dx * 0.75, y+dy * 0.8);  // 过渡点1
-                borderDots.at(5)->setPos(x+dx * 0.5, y+dy);          // 过渡点2
-                borderDots.at(6)->setPos(x+dx * 0.25, y+dy * 1.2); // 过渡点3
-            }
-            if(index==3){
-                borderDots.at(2)->setPos(x+0, y+dy);
-                borderDots.at(0)->setPos(x+dx, y+0);
+    //             // 过渡点
+    //             borderDots.at(4)->setPos(x+dx * 0.75, y+dy * 0.8);  // 过渡点1
+    //             borderDots.at(5)->setPos(x+dx * 0.5, y+dy);          // 过渡点2
+    //             borderDots.at(6)->setPos(x+dx * 0.25, y+dy * 1.2); // 过渡点3
+    //         }
+    //         if(index==3){
+    //             borderDots.at(2)->setPos(x+0, y+dy);
+    //             borderDots.at(0)->setPos(x+dx, y+0);
 
-                // 过渡点
-                borderDots.at(4)->setPos(x+dx * 0.25, y+dy * 0.8);  // 过渡点1
-                borderDots.at(5)->setPos(x+dx * 0.5, y+dy);          // 过渡点2
-                borderDots.at(6)->setPos(x+dx * 0.75, y+dy * 1.2); // 过渡点3
-            }
-        }
-        else if(!inBorder(index)[0]&&inBorder(index)[1]){//仅x越界
-            qDebug()<<"仅x越界";
-            dx = deltax[index];
-            double x = controlDots.at(oppIdx)->scenePos().x();
-            double y = controlDots.at(index)->scenePos().y();
-            double oppY = controlDots.at(oppIdx)->scenePos().y();
-            controlDots.at(index)->setPos(x+dx,y+dy);
-            if(index==0){
-                borderDots.at(1)->setPos(x+0, y+dy);
-                borderDots.at(3)->setPos(x+dx, oppY+0);
+    //             // 过渡点
+    //             borderDots.at(4)->setPos(x+dx * 0.25, y+dy * 0.8);  // 过渡点1
+    //             borderDots.at(5)->setPos(x+dx * 0.5, y+dy);          // 过渡点2
+    //             borderDots.at(6)->setPos(x+dx * 0.75, y+dy * 1.2); // 过渡点3
+    //         }
+    //     }
+    //     else if(!inBorder(index)[0]&&inBorder(index)[1]){//仅x越界
+    //         qDebug()<<"仅x越界";
+    //         dx = deltax[index];
+    //         double x = controlDots.at(oppIdx)->scenePos().x();
+    //         double y = controlDots.at(index)->scenePos().y();
+    //         double oppY = controlDots.at(oppIdx)->scenePos().y();
+    //         controlDots.at(index)->setPos(x+dx,y+dy);
+    //         if(index==0){
+    //             borderDots.at(1)->setPos(x+0, y+dy);
+    //             borderDots.at(3)->setPos(x+dx, oppY+0);
 
-                // 过渡点
-                borderDots.at(4)->setPos(x+dx * 0.25, 0.2*y+0.8*oppY+dy * 0.2);  // 过渡点1
-                borderDots.at(5)->setPos(x+dx * 0.5, oppY+0);          // 过渡点2
-                borderDots.at(6)->setPos(x+dx * 0.75, 1.2*oppY-0.2*y-dy * 0.2); // 过渡点3
-            }
-            if(index==1){
-                borderDots.at(0)->setPos(x+0, y+dy);
-                borderDots.at(2)->setPos(x+dx, oppY+0);
+    //             // 过渡点
+    //             borderDots.at(4)->setPos(x+dx * 0.25, 0.2*y+0.8*oppY+dy * 0.2);  // 过渡点1
+    //             borderDots.at(5)->setPos(x+dx * 0.5, oppY+0);          // 过渡点2
+    //             borderDots.at(6)->setPos(x+dx * 0.75, 1.2*oppY-0.2*y-dy * 0.2); // 过渡点3
+    //         }
+    //         if(index==1){
+    //             borderDots.at(0)->setPos(x+0, y+dy);
+    //             borderDots.at(2)->setPos(x+dx, oppY+0);
 
-                // 过渡点
-                borderDots.at(4)->setPos(x+dx * 0.75, 0.2*y+0.8*oppY+dy * 0.2);  // 过渡点1
-                borderDots.at(5)->setPos(x+dx * 0.5, oppY+0);          // 过渡点2
-                borderDots.at(6)->setPos(x+dx * 0.25, 1.2*oppY-0.2*y-dy * 0.2); // 过渡点3
-            }
-            if(index==2){
-                borderDots.at(3)->setPos(x+0, y+dy);
-                borderDots.at(1)->setPos(x+dx, oppY+0);
+    //             // 过渡点
+    //             borderDots.at(4)->setPos(x+dx * 0.75, 0.2*y+0.8*oppY+dy * 0.2);  // 过渡点1
+    //             borderDots.at(5)->setPos(x+dx * 0.5, oppY+0);          // 过渡点2
+    //             borderDots.at(6)->setPos(x+dx * 0.25, 1.2*oppY-0.2*y-dy * 0.2); // 过渡点3
+    //         }
+    //         if(index==2){
+    //             borderDots.at(3)->setPos(x+0, y+dy);
+    //             borderDots.at(1)->setPos(x+dx, oppY+0);
 
-                // 过渡点
-                borderDots.at(4)->setPos(x+dx * 0.75, 0.2*oppY+0.8*y+dy * 0.8);  // 过渡点1
-                borderDots.at(5)->setPos(x+dx * 0.5, y+dy);          // 过渡点2
-                borderDots.at(6)->setPos(x+dx * 0.25, 1.2*y-0.2*oppY+dy * 1.2); // 过渡点3
-            }
-            if(index==3){
-                borderDots.at(2)->setPos(x+0, y+dy);
-                borderDots.at(0)->setPos(x+dx, oppY+0);
+    //             // 过渡点
+    //             borderDots.at(4)->setPos(x+dx * 0.75, 0.2*oppY+0.8*y+dy * 0.8);  // 过渡点1
+    //             borderDots.at(5)->setPos(x+dx * 0.5, y+dy);          // 过渡点2
+    //             borderDots.at(6)->setPos(x+dx * 0.25, 1.2*y-0.2*oppY+dy * 1.2); // 过渡点3
+    //         }
+    //         if(index==3){
+    //             borderDots.at(2)->setPos(x+0, y+dy);
+    //             borderDots.at(0)->setPos(x+dx, oppY+0);
 
-                // 过渡点
-                borderDots.at(4)->setPos(x+dx * 0.25, 0.2*oppY+0.8*y+dy * 0.8);  // 过渡点1
-                borderDots.at(5)->setPos(x+dx * 0.5, y+dy);          // 过渡点2
-                borderDots.at(6)->setPos(x+dx * 0.75, 1.2*y-0.2*oppY+dy * 1.2); // 过渡点3
-            }
-        }
-        else if(inBorder(index)[0] && !inBorder(index)[1]){//仅y越界
-            qDebug()<<"y越界";
-            dy = deltay[index];
-            double x=controlDots.at(index)->scenePos().x();
-            double oppX=controlDots.at(oppIdx)->scenePos().x();
-            double y=controlDots.at(oppIdx)->scenePos().y();
+    //             // 过渡点
+    //             borderDots.at(4)->setPos(x+dx * 0.25, 0.2*oppY+0.8*y+dy * 0.8);  // 过渡点1
+    //             borderDots.at(5)->setPos(x+dx * 0.5, y+dy);          // 过渡点2
+    //             borderDots.at(6)->setPos(x+dx * 0.75, 1.2*y-0.2*oppY+dy * 1.2); // 过渡点3
+    //         }
+    //     }
+    //     else if(inBorder(index)[0] && !inBorder(index)[1]){//仅y越界
+    //         qDebug()<<"y越界";
+    //         dy = deltay[index];
+    //         double x=controlDots.at(index)->scenePos().x();
+    //         double oppX=controlDots.at(oppIdx)->scenePos().x();
+    //         double y=controlDots.at(oppIdx)->scenePos().y();
 
-            controlDots.at(index)->setPos(x+dx,y+dy);
-            if(index==0){
-                borderDots.at(1)->setPos(oppX+0, y+dy);
-                borderDots.at(3)->setPos(x+dx, y+0);
+    //         controlDots.at(index)->setPos(x+dx,y+dy);
+    //         if(index==0){
+    //             borderDots.at(1)->setPos(oppX+0, y+dy);
+    //             borderDots.at(3)->setPos(x+dx, y+0);
 
-                // 过渡点
-                borderDots.at(4)->setPos(0.75*oppX+0.25*x+dx * 0.25, y+dy * 0.2);  // 过渡点1
-                borderDots.at(5)->setPos(0.5*oppX+0.5*x+dx * 0.5, y+0);          // 过渡点2
-                borderDots.at(6)->setPos(0.25*oppX+0.75*x+dx * 0.75, y-dy * 0.2); // 过渡点3
-            }
-            if(index==1){
-                borderDots.at(0)->setPos(oppX+0, y+dy);
-                borderDots.at(2)->setPos(x+dx, y+0);
+    //             // 过渡点
+    //             borderDots.at(4)->setPos(0.75*oppX+0.25*x+dx * 0.25, y+dy * 0.2);  // 过渡点1
+    //             borderDots.at(5)->setPos(0.5*oppX+0.5*x+dx * 0.5, y+0);          // 过渡点2
+    //             borderDots.at(6)->setPos(0.25*oppX+0.75*x+dx * 0.75, y-dy * 0.2); // 过渡点3
+    //         }
+    //         if(index==1){
+    //             borderDots.at(0)->setPos(oppX+0, y+dy);
+    //             borderDots.at(2)->setPos(x+dx, y+0);
 
-                // 过渡点
-                borderDots.at(4)->setPos(0.75*x+0.25*oppX+dx * 0.75, y+dy * 0.2);  // 过渡点1
-                borderDots.at(5)->setPos(0.5*x+0.5*oppX+dx * 0.5, y+0);          // 过渡点2
-                borderDots.at(6)->setPos(0.25*x+0.75*oppX+dx * 0.25, y-dy * 0.2); // 过渡点3
-            }
-            if(index==2){
-                borderDots.at(3)->setPos(oppX+0, y+dy);
-                borderDots.at(1)->setPos(x+dx, y+0);
+    //             // 过渡点
+    //             borderDots.at(4)->setPos(0.75*x+0.25*oppX+dx * 0.75, y+dy * 0.2);  // 过渡点1
+    //             borderDots.at(5)->setPos(0.5*x+0.5*oppX+dx * 0.5, y+0);          // 过渡点2
+    //             borderDots.at(6)->setPos(0.25*x+0.75*oppX+dx * 0.25, y-dy * 0.2); // 过渡点3
+    //         }
+    //         if(index==2){
+    //             borderDots.at(3)->setPos(oppX+0, y+dy);
+    //             borderDots.at(1)->setPos(x+dx, y+0);
 
-                // 过渡点
-                borderDots.at(4)->setPos(0.75*x+0.25*oppX+dx * 0.75, y+dy * 0.8);  // 过渡点1
-                borderDots.at(5)->setPos(0.5*x+0.5*oppX+dx * 0.5, y+dy);          // 过渡点2
-                borderDots.at(6)->setPos(0.25*x+0.75*oppX+dx * 0.25, y+dy * 1.2); // 过渡点3
-            }
-            if(index==3){
-                borderDots.at(2)->setPos(oppX+0, y+dy);
-                borderDots.at(0)->setPos(x+dx, y+0);
+    //             // 过渡点
+    //             borderDots.at(4)->setPos(0.75*x+0.25*oppX+dx * 0.75, y+dy * 0.8);  // 过渡点1
+    //             borderDots.at(5)->setPos(0.5*x+0.5*oppX+dx * 0.5, y+dy);          // 过渡点2
+    //             borderDots.at(6)->setPos(0.25*x+0.75*oppX+dx * 0.25, y+dy * 1.2); // 过渡点3
+    //         }
+    //         if(index==3){
+    //             borderDots.at(2)->setPos(oppX+0, y+dy);
+    //             borderDots.at(0)->setPos(x+dx, y+0);
 
-                // 过渡点
-                borderDots.at(4)->setPos(0.75*oppX+0.25*x+dx * 0.25+dx * 0.25, y+dy * 0.8);  // 过渡点1
-                borderDots.at(5)->setPos(0.5*oppX+0.5*x+dx * 0.5, y+dy);          // 过渡点2
-                borderDots.at(6)->setPos(0.25*oppX+0.75*x+dx * 0.75, y+dy * 1.2); // 过渡点3
-            }
-        }
-        draw();
-        return;
-    }
+    //             // 过渡点
+    //             borderDots.at(4)->setPos(0.75*oppX+0.25*x+dx * 0.25+dx * 0.25, y+dy * 0.8);  // 过渡点1
+    //             borderDots.at(5)->setPos(0.5*oppX+0.5*x+dx * 0.5, y+dy);          // 过渡点2
+    //             borderDots.at(6)->setPos(0.25*oppX+0.75*x+dx * 0.75, y+dy * 1.2); // 过渡点3
+    //         }
+    //     }
+    //     draw();
+    //     return;
+    // }
     qDebug() << "开始缩放";
     controlDots.at(index)->moveBy(dx, dy);
 
@@ -286,3 +286,6 @@ FlowDocuElement *FlowDocuElement::deepClone()
 
     return clonedElement;
 }
+
+
+
